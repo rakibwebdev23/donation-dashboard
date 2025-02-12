b35c51eb5aa062f70211be5b591272236fce9559
Rakib <rh.rakibhasan365@gmail.com>
2025-02-12 17:41:46 +0600

    get all projects data by axios and tanStackQuery

---
+++ src/hooks/allProjectsData.jsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
+
+const allProjectsData = () => {
+    const axiosSecure = useAxiosSecure();
+    const { data: projects = [], refetch } = useQuery({
+        queryKey: ["projects"],
+        queryFn: async () => {
+            const res = await axiosSecure.get("/projects");
+            return res.data;
+        }
+    })
+    return [projects, refetch];
+};
+
+export default allProjectsData;
